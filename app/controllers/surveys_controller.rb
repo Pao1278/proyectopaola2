c311cbd0851075e82cbaa0964d2eebf15c369247
rrojasedutecno <90879116+Pao1278@users.noreply.github.com>
2021-09-17 02:18:06 +0000

    despliegue2

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -30,21 +30,7 @@ class SurveysController < ApplicationController
     #Fin Registrar en base de datos
     ###
 
-    pusher = Pusher::Client.new(
-      app_id: '1268275',
-      key: '59b9304c77cd580eb83a',
-      secret: '1b1bfb0bd0e8777e10a3',
-      cluster: 'mt1',
-      encrypted: true
-    )
 
-    noti = {}
-    noti[gesturex] = Survey.where(gesture: gesturex).count
-
-    
-    pusher.trigger('my-channel', 'my-event', {
-      message: noti
-    })
 
     redirect_to  :surveys_thankyou    
   end
